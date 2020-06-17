81a28e322b755c5a577d02920a2ac31828f8c413
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-06-17 13:25:47 +0700

    Rename "accept" negotiation to "MIME" negotiation.

--- src/headers/index.ts
+++ src/headers/index.ts
@@ -1,4 +1,4 @@
-export * from './http-accept-negotiator';
 export * from './http-content-negotiator';
 export * from './http-forward-trust';
 export * from './http-forward-rep';
+export * from './http-mime-negotiator';
