a03d5832fb95c3d47c162de33317e485c3faf1e6
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-01-27 17:00:12 +0700

    Add missing export

--- src/headers/index.ts
+++ src/headers/index.ts
@@ -3,7 +3,7 @@
  */
 export * from './http-content-negotiator';
 export * from './http-encoding-negotiator';
-export * from './http-forward-trust';
 export * from './http-forward-rep';
+export * from './http-forward-trust';
 export * from './http-language-negotiator';
 export * from './http-mime-negotiator';
